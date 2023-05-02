b675b72fd4b971ad3c59ed64172a446aa0c34592
Kamar <57263951+Kamaruddheen@users.noreply.github.com>
2023-05-02 21:05:50 +0530

    Added session-9 js password assignment

--- Week-3/Session-9/JavaScript password strength/script.js
+++ Week-3/Session-9/JavaScript password strength/script.js
@@ -1,1 +1,17 @@
 // Your solution goes here 
+function isStrongPassword(password) {
+    if (password.length < 8)
+        return false    
+    if (password.search("password") == 0)
+        return false
+    var regex = new RegExp("(?=.*[A-Z])")
+    if (!regex.test(password))
+        return false
+    
+    return true;
+}
+
+isStrongPassword("Qwerty")          // false - Too short 
+isStrongPassword("passwordQwerty")  // false - Contains "password" 
+isStrongPassword("qwerty123")       // false - No uppercase characters 
+isStrongPassword("Qwerty123")       // true
