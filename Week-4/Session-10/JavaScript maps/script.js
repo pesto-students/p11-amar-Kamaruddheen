36ffe394e363f51cf44ad010a84cbee85db87bf6
Kamar <57263951+Kamaruddheen@users.noreply.github.com>
2023-05-14 15:59:59 +0530

    Added session-10 Maps assignments
    Finding the number of words frequency using maps.

--- Week-4/Session-10/JavaScript maps/script.js
+++ Week-4/Session-10/JavaScript maps/script.js
@@ -1,3 +1,24 @@
 function calcWordFrequencies() {
-   
+    
+    var sentence = prompt("Enter a sentence : ")
+    // "hey hi Mark hi mark"
+
+    const words_frequencies = new Map();
+
+    sentence.split(" ").forEach(element => {
+
+        // Existing Word
+        if (words_frequencies.has(element)) {            
+            words_frequencies.set(element, words_frequencies.get(element) + 1)
+        }
+        // New Words
+        else {
+            
+            words_frequencies.set(element, 1)
+        }
+    });
+
+    console.log(words_frequencies);
 }
+
+calcWordFrequencies()
